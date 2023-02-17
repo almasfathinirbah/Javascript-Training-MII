1718d95c2bc384148fdbd980e0e47ba49a4f5a26
almasfathinirbah <85482667+almasfathinirbah@users.noreply.github.com>
2023-02-17 13:37:51 +0700

    Update 00015.js

--- Tugas Javascript - Almas Fahin Irbah/00015.js
+++ Tugas Javascript - Almas Fahin Irbah/00015.js
@@ -12,11 +12,8 @@ function tentukanDeretAritmatika(arr) {
     if (n == 1)
       return true;
   
-    // Sort array
     arr.sort((a, b) => a - b);
-  
-    // After sorting, difference between
-    // consecutive elements must be same.
+    
     let d = arr[1] - arr[0];
     for (let i=2; i<n; i++)
       if (arr[i] - arr[i-1] != d)
